af9fa4f045a995c04d9520796b290a1fe6a14226
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-02 13:39:43 -0700

    Update login page to use magic link rather

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,14 +1,37 @@
-import { login, signup } from "./actions";
+"use client";
+
+import { createClient } from "@supabase/supabase-js";
+import { Auth } from "@supabase/auth-ui-react";
+import {
+  // Import predefined theme
+  ThemeSupa,
+} from "@supabase/auth-ui-shared";
+
+const supabase = createClient(
+  "https://pzhwgamtivczmertcbcj.supabase.co",
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InB6aHdnYW10aXZjem1lcnRjYmNqIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTM4NTQ2OTksImV4cCI6MjAyOTQzMDY5OX0.s7oAQsDMvFv4tvCJt0aHpGnJYt8upTi2YEqNrS6ijA8"
+);
 
 export default function LoginPage() {
   return (
-    <form>
-      <label htmlFor="email">Email:</label>
-      <input id="email" name="email" type="email" required />
-      <label htmlFor="password">Password:</label>
-      <input id="password" name="password" type="password" required />
-      <button formAction={login}>Log in</button>
-      <button formAction={signup}>Sign up</button>
-    </form>
+      <Auth
+        supabaseClient={supabase}
+        // show magic link sign-in option
+        view="magic_link"
+        appearance={{
+          theme: ThemeSupa,
+          style: {
+            divider: {
+              background: "#d9d9d9",
+            },
+          },
+        }}
+        providers={
+          [
+          ]
+        }
+        redirectTo="http://localhost:3000/auth/callback"
+      />
+    </>
   );
 }
