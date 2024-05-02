7713a4acee21877dbcb6fe89b6aec48258fd71da
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-02 16:40:07 -0700

    Update actions.ts

--- app/login/actions.ts
+++ app/login/actions.ts
@@ -12,30 +12,9 @@ export async function login(formData: FormData) {
   // in practice, you should validate your inputs
   const data = {
     email: formData.get("email") as string,
-    password: formData.get("password") as string,
   };
 
-  const { error } = await supabase.auth.signInWithPassword(data);
-
-  if (error) {
-    redirect("/error");
-  }
-
-  revalidatePath("/", "layout");
-  redirect("/account");
-}
-
-export async function signup(formData: FormData) {
-  const supabase = createClient();
-
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
-  const data = {
-    email: formData.get("email") as string,
-    password: formData.get("password") as string,
-  };
-
-  const { error } = await supabase.auth.signUp(data);
+  const { error } = await supabase.auth.signInWithOtp(data);
 
   if (error) {
     redirect("/error");
