a1cebc03bd7c7d86d4501d8e644fabd5b2dceb2e
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-06 02:14:02 -0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 
 
+const App: FC = () => {
 
       </div>
+  return (
+    <div
+      className={clsx("h-screen w-screen", {
+        "bg-neutral-200": darkMode,
+        "bg-neutral-700": !darkMode,
+      })}
+    >
+    </div>
   );
+};
+
+export default App;
