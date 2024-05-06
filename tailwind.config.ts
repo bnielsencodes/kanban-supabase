6b3ac83860242502b9a85969f72cf1a1384f1296
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-06 02:09:30 -0700

    Add color variables

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,22 @@ const config: Config = {
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    colors: {
+      neutral: {
+        100: "#000112",
+        200: "#20212c",
+        300: "#2b2c37",
+        400: "#3e3f4e",
+        500: "#828fa3",
+        600: "#e4ebfa",
+        700: "#f4f7fd",
+        800: "#fff",
+      },
+      primary: "#635fc7",
+      "primary-hover": "#a8a4ff",
+      warning: "#ea5555",
+      "warning-hover": "#ff9898",
+    },
     extend: {
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
