a1ae0f396f225c939969ab5ac44182124cd11a27
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 14:14:13 -0700

    Import Checkbox.tsx

--- app/_components/taskboard/Subtasks.tsx
+++ app/_components/taskboard/Subtasks.tsx
@@ -1,8 +1,13 @@
+import Checkbox from "../Checkbox";
+
 export default function Subtasks() {
   return (
     <div className="subtasks">
       <p className="subtasks--title">{`Subtasks (0 of 0)`}</p>
       <div>
+        <Checkbox />
+        <Checkbox />
+        <Checkbox />
       </div>
     </div>
   );
