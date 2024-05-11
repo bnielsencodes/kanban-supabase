050bc6f69e00b75d0ff9988fb64c13bf47354ba7
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 15:34:44 -0700

    Create cols state

--- app/_components/taskboard/CurrentBoard.tsx
+++ app/_components/taskboard/CurrentBoard.tsx
@@ -1,5 +1,9 @@
+import { useState } from "react";
 import data from "../../data";
+
 export default function TaskBoard() {
+  const [cols] = useState(data[0].columns);
+
   return (
     <>
       {cols.length > 0 ? (
