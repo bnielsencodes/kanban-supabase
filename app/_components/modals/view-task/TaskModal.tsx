9bdd9dfb46c5e5e01bfebe4df78c9ba3b5bff4aa
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 14:13:47 -0700

    Create TaskModal.tsx

---
+++ app/_components/modals/view-task/TaskModal.tsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import ellipsisIcon from "/public/assets/icon-vertical-ellipsis.svg";
+
+export default function TaskModal() {
+  return (
+    <div className="task-modal">
+      <div className="task-modal--title-container">
+        <p className="task-modal--title">
+          Research pricing points of various competitors and trial different
+          business models
+        </p>
+        <Image
+          className="w-6 h-[25px] mr-4 "
+          src={ellipsisIcon}
+          alt="veritcal ellipsis icon"
+          width="0"
+          height="0"
+          sizes="100vw"
+        />
+      </div>
+      <p className="task--description">
+        We know what we<span>&apos</span>re planning to build for version one.
+        Now we need to finalize the first pricing model we<span>&apos</span>ll
+        use. Keep iterating the subtasks until we have a coherent proposition.
+      </p>
+      <div className="current-status">
+        <label htmlFor="select-status">Current Status</label>
+        <select name="" id="select-status">
+          <option value="doing">Doing</option>
+          <option value="not-doing">Todo</option>
+        </select>
+      </div>
+    </div>
+  );
+}
