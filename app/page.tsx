b3d7a030e30ac14b4cf98f5d675c3ba551b1d1b9
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 13:36:38 -0700

    Create loader spinner and show until mounted

--- app/page.tsx
+++ app/page.tsx
@@ -12,6 +12,7 @@ const App: FC = () => {
   const supabase = createClient();
   const [session, setSession] = useState<Session | null>(null);
   const [darkMode, setDarkMode] = useState(true);
+  const [mounted, setMounted] = useState(false);
 
   useEffect(() => {
     supabase.auth.getSession().then(({ data: { session } }: any) => {
@@ -29,7 +30,25 @@ const App: FC = () => {
     // window.localStorage.setItem("theme", htmlElement.dataset.theme);
   }
 
+  // useEffect only runs on the client, so now we can safely show the UI
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div
+        className={clsx("flex h-screen w-screen items-center justify-center", {
+          "bg-neutral-200": darkMode,
+          "bg-neutral-700": !darkMode,
+        })}
+      >
+        {/* loader */}
+        <span className="loader relative h-12 w-12 overflow-hidden rounded-full bg-primary shadow-[0_5px_12px_rgba(0,0,0,.15),inset_0_0_30px_4px_rgba(0,0,0,.15)] before:absolute before:top-[-40%] before:h-full before:w-full before:animate-wave before:rounded-[30%] before:bg-neutral-500 before:content-[''] after:absolute after:top-[-40%] after:h-full after:w-full after:animate-wave after:rounded-[45%] after:bg-neutral-600 after:content-[''] lg:h-16 lg:w-16"></span>
       </div>
+    );
+  }
+
   return (
     <div
       className={clsx("h-screen w-screen", {
