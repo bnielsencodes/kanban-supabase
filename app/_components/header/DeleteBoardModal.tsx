a6c89fdd3d81210d91d3d6c6a8bd19855009f3e6
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 13:54:04 -0700

    Create DeleteBoardModal.tsx

---
+++ app/_components/header/DeleteBoardModal.tsx
@@ -0,0 +1,64 @@
+export default function DeleteBoardModal({
+  darkMode,
+  currentBoard,
+  setShowDeleteBoardModal,
+  handleRemoveBoard,
+}: {
+  darkMode: boolean;
+  currentBoard: {
+    id: number;
+    name: string;
+    columns: {
+      id: number;
+      name: string;
+      tasks: {
+        id: number;
+        title: string;
+        description: string;
+        status: string;
+        subtasks: {
+          id: number;
+          title: string;
+          isCompleted: boolean;
+        }[];
+      }[];
+    }[];
+  };
+  setShowDeleteBoardModal: React.Dispatch<React.SetStateAction<boolean>>;
+  handleRemoveBoard: (id: number) => void;
+}) {
+  return (
+    <div className="absolute left-0 top-0 z-10 h-screen w-screen select-none">
+      <div className="bg-black/40 absolute left-0 top-0 h-full w-full"></div>
+      <div
+        className={`absolute left-1/2 top-[calc(50%+64px)] z-20 w-[343px] -translate-x-1/2 -translate-y-1/2 rounded-md p-6 ${
+          darkMode ? "bg-neutral-300" : "bg-neutral-800"
+        } select-none`}
+      >
+        <p className="mb-[22px] text-lg font-bold leading-[23px] text-warning">
+          Delete this board?
+        </p>
+        <p className="mb-[22px] pr-1 text-[13px] leading-[23px] text-neutral-500">
+          Are you sure you want to delete the ‘{currentBoard.name}’ board? This
+          action will remove all columns and tasks and cannot be reversed.
+        </p>
+        <div className="flex flex-col gap-4">
+          <button
+            className={`h-[40px] cursor-pointer rounded-[20px] bg-warning text-[13px] font-semibold leading-[13px] text-neutral-800`}
+            onClick={() => handleRemoveBoard(currentBoard.id)}
+          >
+            Delete
+          </button>
+          <button
+            className="h-[40px] cursor-pointer rounded-[20px] bg-neutral-800 text-[13px] font-semibold leading-[13px] text-primary"
+            onClick={(e) => {
+              setShowDeleteBoardModal(false);
+            }}
+          >
+            Cancel
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+}
