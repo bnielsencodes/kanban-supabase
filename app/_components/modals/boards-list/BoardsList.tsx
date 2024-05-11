196afb9ec2b5dea6727ee3dbeb9a4007b5389e2c
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 13:52:48 -0700

    Map placeholderData and render boardListItems

--- app/_components/modals/boards-list/BoardsList.tsx
+++ app/_components/modals/boards-list/BoardsList.tsx
@@ -51,11 +51,56 @@ export default function BoardsList({
   };
   setCurrentBoard: React.Dispatch<React.SetStateAction<any>>;
 }) {
+  const boardsListItems = placeholderData.map((board) => {
+    // change current board on click
+    function handleClick() {
+      setCurrentBoard(data[board.id - 1]);
+      setShowBoardsModal(false);
+    }
+
+    return (
+      <li
+        className={`flex h-12 w-60 items-center rounded-r-[100px] pl-6 ${
+          currentBoard.name === board.name ? "bg-primary" : ""
+        }`}
+        key={board.id}
+      >
+        <svg
+          className="mr-3"
+          width="16"
+          height="16"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            className={`${
+              currentBoard.name === board.name
+                ? "fill-neutral-800"
+                : "fill-neutral-500"
+            }`}
+            d="M0 2.889A2.889 2.889 0 0 1 2.889 0H13.11A2.889 2.889 0 0 1 16 2.889V13.11A2.888 2.888 0 0 1 13.111 16H2.89A2.889 2.889 0 0 1 0 13.111V2.89Zm1.333 5.555v4.667c0 .859.697 1.556 1.556 1.556h6.889V8.444H1.333Zm8.445-1.333V1.333h-6.89A1.556 1.556 0 0 0 1.334 2.89V7.11h8.445Zm4.889-1.333H11.11v4.444h3.556V5.778Zm0 5.778H11.11v3.11h2a1.556 1.556 0 0 0 1.556-1.555v-1.555Zm0-7.112V2.89a1.555 1.555 0 0 0-1.556-1.556h-2v3.111h3.556Z"
+          />
+        </svg>
+        <p
+          className={`h-[16px] ${
+            currentBoard.name === board.name
+              ? "text-neutral-800"
+              : "text-neutral-500"
+          } cursor-pointer font-semibold leading-[19px]`}
+          onClick={handleClick}
+        >
+          {board.name}
+        </p>
+      </li>
+    );
+  });
+
   return (
     <div>
       <p className="pl-6 text-xs font-semibold uppercase leading-[15px] tracking-[2.4px] text-neutral-500">
         All Boards ({placeholderData.length})
       </p>
+      <ul className="mb-[14px] mt-[18px]">{boardsListItems}</ul>
+
       <div className="mb-[34px] flex items-center pl-6">
         <svg
           className="mr-3"
