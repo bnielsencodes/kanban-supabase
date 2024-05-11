428c82ffa08920031123d5adfdb454a4678a5c94
Brandon Nielsen <57652759+bnielsencodes@users.noreply.github.com>
2024-05-11 14:14:01 -0700

    Create Subtasks.tsx

---
+++ app/_components/taskboard/Subtasks.tsx
@@ -0,0 +1,9 @@
+export default function Subtasks() {
+  return (
+    <div className="subtasks">
+      <p className="subtasks--title">{`Subtasks (0 of 0)`}</p>
+      <div>
+      </div>
+    </div>
+  );
+}
